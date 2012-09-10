522661bd0f706a13932e4c317e865de170a96253
Andrew Serputko <aserputko@gmail.com>
2012-09-10 17:47:12 +0300

    Implement task The system can retrieve a ranked list of most popular items (most often ordered items).  -- In Progress

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :price, :presence => true
+
+  # TODO: not implemented yet. This is quick solution. 
+  def self.top
+  	top_items = []
+  	Order.all.each { |order| order.items.each { |item| top_items << item.name }}
+  	top_items.uniq
+  end
 end
